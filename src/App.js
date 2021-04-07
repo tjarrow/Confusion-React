d3ab3dd346549056d9221f0eaca787cb0ef389d3
tjarrow <nickita0104@gmail.com>
2021-04-07 23:49:33 +0300

    Before Assignment 3

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Menu from './components/MenuComponent';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 class App extends Component {
 
@@ -18,11 +22,13 @@ class App extends Component {
 
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-            <Main/>
-        </div>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+              <Main/>
+          </div>
+        </BrowserRouter>
+      </Provider>
     );
   }
 }
